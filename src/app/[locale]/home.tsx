f475c28424bef7eb20af1232afbd61a44ce86131
Adrien Guesnel <adrien-guesnel@users.noreply.github.com>
2025-12-21 18:40:09 +0100

    feat: redo footer

--- src/app/[locale]/home.tsx
+++ src/app/[locale]/home.tsx
@@ -2,10 +2,10 @@
 
 import Articles from "@components/organisms/Articles";
 import Contact from "@components/organisms/Contact";
+import Footer from "@components/organisms/Footer";
 import Hero from "@components/organisms/Hero";
 import NavigationBar from "@components/organisms/NavigationBar";
 import Skills from "@components/organisms/Skills";
-import Footer from "@/src/app/components/Footer";
 
 export default function Home({ locale }: { locale: string }) {
   return (
